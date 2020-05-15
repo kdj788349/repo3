321428d91cc512f251bb7755d0618afa73af5c51
kdj <1395253746@qq.com>
2020-05-15 18:11:05 +0800

    提交git项目

---
+++ git_test/src/com/hnist/FistProject.java
@@ -0,0 +1,16 @@
+package com.hnist;
+
+public class FistProject {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+    }
+
+    public void test(){
+        int i=1;
+        i+=10;
+    }
+}
