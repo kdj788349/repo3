43fbf5c079ee8cbc5ceaa18ce376b07d9af3cfee
kdj <1395253746@qq.com>
2020-05-15 18:16:04 +0800

    提交git修改

--- git_test/src/com/hnist/FistProject.java
+++ git_test/src/com/hnist/FistProject.java
@@ -8,9 +8,10 @@ public class FistProject {
         System.out.println("hello world!");
         System.out.println("hello world!");
     }
-
+      //hello
     public void test(){
         int i=1;
         i+=10;
+        i++;
     }
 }
